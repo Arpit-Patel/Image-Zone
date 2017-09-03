27c76e0976695284566255c42244acf1f2c3cf7a
Arpit Patel <apt@Arpits-MacBook-Air.local>
2017-09-03 02:54:37 -0400

    fix

--- app.py
+++ app.py
@@ -97,10 +97,10 @@ def render():
 
 if __name__ == "__main__":
     app.run(port=8000)
-    '''detection_graph = tf.Graph()
+    detection_graph = tf.Graph()
     with detection_graph.as_default():
         od_graph_def = tf.GraphDef()
         with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
             serialized_graph = fid.read()
             od_graph_def.ParseFromString(serialized_graph)
-            tf.import_graph_def(od_graph_def, name='')'''
+            tf.import_graph_def(od_graph_def, name='')
